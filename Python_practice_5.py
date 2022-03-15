722c0203986178d7cade094299f980216d689184
Bluedog11 <sheilamunsell@hotmail.com>
2022-03-15 12:04:49 -0500

    Adding PyPoll.py file

--- Python_practice_5.py
+++ Python_practice_5.py
@@ -3,8 +3,10 @@ for county, voters in counties_dict.items():
     print (f"{county} county has {voters:,} registered voters.")
 
 voting_data=[{"county":"Arapahoe","registered_voters":422829},{"county":"Denver","registered_voters":463353},{"county":"Jefferson","registered_voters":432438}]
-for county_dict in voting_data:
-    for i in range (len(voting_data)):
-        print (voting_data[i]f'[{county} county'])
+for i in range(len(voting_data)):
+    print(f"{voting_data[i]['county']} county has {voting_data[i]['registered_voters']:,} registered voters.")
+        
+    
+
     
     
