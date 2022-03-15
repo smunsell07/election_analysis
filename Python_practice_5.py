7ed6b62ae7ac13abc2598dcfb19483305b1e61cf
Bluedog11 <sheilamunsell@hotmail.com>
2022-03-14 23:21:03 -0500

    initial upload

---
+++ Python_practice_5.py
@@ -0,0 +1,10 @@
+counties_dict={"Arapahoe":422829,"Denver":463353,"Jefferson":432438}
+for county, voters in counties_dict.items():
+    print (f"{county} county has {voters:,} registered voters.")
+
+voting_data=[{"county":"Arapahoe","registered_voters":422829},{"county":"Denver","registered_voters":463353},{"county":"Jefferson","registered_voters":432438}]
+for county_dict in voting_data:
+    for i in range (len(voting_data)):
+        print (voting_data[i]f'[{county} county'])
+    
+    
